81e49b1858c1f8af808429d8d0401b5f74ece702
sergei <uraniumcore238@gmail.com>
2021-02-26 09:49:30 +0500

    added capabilities

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -19,6 +19,7 @@ public class TestBase {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud:4444/wd/hub/";
     }
 
